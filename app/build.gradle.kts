ef8023c780f6678e933dc4734246fbdb86e49d9f
artemzaharov <artem4ikaz@icloud.com>
2024-05-25 14:44:57 +0300

    get

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,12 +33,15 @@ android {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation(libs.firebase.bom)
+    //noinspection UseTomlInstead
     implementation("com.google.firebase:firebase-analytics")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
